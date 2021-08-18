c9c5d54300d94ba475c9e7667d0a6d096332a83c
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-08-18 23:43:52 +0300

    added phone search

--- automation/automation.py
+++ automation/automation.py
@@ -4,7 +4,17 @@ with open('assets/potential-contacts.txt','r') as file:
     file = file.read()
     email_searh = re.findall("[\w.+-]+@[\w-]+\.[\w.-]+", file)
     # print(email_searh)
-    with open('assets/Email-Searched.txt','w+') as file:
-        for email in email_searh:
-            file.write(f'{email_searh}')
-    print(email_searh)
+    sorted_email = sorted(email_searh)
+    with open('assets/email-Searched.txt','w+') as file:
+        for email in sorted_email:
+            file.write(f'{sorted_email}')
+    print(sorted_email)
+
+with open('assets/potential-contacts.txt','r') as file:
+    file = file.read()
+    phone_searh = re.findall("(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})", file)
+    print(phone_searh)
+    with open('assets/phone-Searched.txt','w+') as file:
+        for phone in phone_searh:
+            file.write(f'{phone_searh}')
+    print(phone_searh)
